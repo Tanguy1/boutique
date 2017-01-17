d672a53f1038df91ca0f32934ea3d6681a1dfaeb
Administrateur <Administrateur@M2i-E4332>
2017-01-17 16:59:44 +0100

    Version Initiale

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Inscription</title>
+    </head>
+    <body>
+        <form>
+            <label>Nom</label><input type="text" name="Nom"><br>
+            <label>Prenom</label><input type="text" name="Prenom"><br>
+            <label>Identifiant</label><input type="text" name="identifiant"><br>
+            <label>Mot de Pass</label><input type="password" name="mdp1"><br>
+            <label>Confirmer Mot de Pass</label><input type="password" name="mdp2"><br>
+            <label>Telephone</label><input type="tel" name="tel"><br>
+            <label>E-mail</label><input type="email" name="email"><br>
+            <label>Adresse</label><textarea type="text" name="adresse"></textarea><br>
+            <a href="connexion.php">Déjà Client</a><br>
+            <input type="button" value="Créer un Compte">
+        </form>
+    </body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Inscription</title>
+    </head>
+    <body>
+        <?php
+            session_start()
+            
+        ?>
+    </body>
+</html>
